c13314a2e8272685db6bf8ce0e6876b494b0f6df
Arun1457 <meet2arunpatel@gmail.com>
2026-04-26 21:20:00 +0530

    fix: add API keys to dashboard

--- backend/dashboard.py
+++ backend/dashboard.py
@@ -43,9 +43,8 @@ FEAT_FILE  = os.path.join(BASE_DIR, "models",  "feature_columns.pkl")
 # ─────────────────────────────────────────────
 # THREAT INTELLIGENCE
 # ─────────────────────────────────────────────
-VT_API_KEY     = "YOUR_VIRUSTOTAL_API_KEY"   # replace with yours
-SHODAN_API_KEY = "YOUR_SHODAN_API_KEY"        # replace with yours
-
+VT_API_KEY     = "612547e9b2a28046f6d3241fa960850eca22aecaecb8b7483136c1ad019fd617"   
+SHODAN_API_KEY = "4pqsK9TmDwXirvriNOyPTcAnlpg0tPSG"        
 def vt_check_ip(ip):
     try:
         r = requests.get(
